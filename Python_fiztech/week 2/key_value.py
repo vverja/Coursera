b7d2a0c5aba9f8fa399697b595d07be7747a8a41
dev <vverja@gmail.com>
2019-02-13 00:28:55 +0200

    working key-value

--- Python_fiztech/week 2/key_value.py
+++ Python_fiztech/week 2/key_value.py
@@ -7,14 +7,18 @@ import json
 parser = argparse.ArgumentParser()
 parser.add_argument("--key")
 parser.add_argument("--value")
-args = parser.parse_args(['--key', '1', '--value', 'Second'])
+args = parser.parse_args(['--key', '1'])
 
 temp_file = path.join(tempfile.gettempdir(), 'storage.json')
 obj = {}
-with open(temp_file, 'w+') as f:
-    some_string = f.read()
-    if some_string:
-        obj = json.loads(some_string)
+f = open(temp_file, 'r')
+obj = json.load(f)
+f.close()
+with open(temp_file, 'w') as f:
+    #some_string = f.read()
+    #if some_string:
+    #    obj = json.loads(some_string)
+    if obj:
         if args.value:
             if args.key in obj:
                 item = obj.get(args.key)
@@ -24,8 +28,13 @@ with open(temp_file, 'w+') as f:
                     obj[args.key] = [item, args.value]
             else:
                 obj[args.key] = args.value
-
+        else:
+            item = obj.get(args.key)
+            if type(item) is list:
+                print(*item, sep=", ")
+            else:
+                print(item)
     elif args.value:
         obj[args.key] = args.value
     json.dump(obj, f)
-print(obj)
+
